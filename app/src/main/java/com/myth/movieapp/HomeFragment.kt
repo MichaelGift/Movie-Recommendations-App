73502f816588beb7caeff756382b0c8e4c3b2c25
Michael Gift <120593172+MichaelGift@users.noreply.github.com>
2023-09-24 22:22:23 +0300

    Update HomeFragment.kt

--- app/src/main/java/com/myth/movieapp/HomeFragment.kt
+++ app/src/main/java/com/myth/movieapp/HomeFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinint var _binding: FragmentHomeBinding
+    private  val binding get() = _binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _bingind =  DatabindingUtil.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding?.root
     }
 }
